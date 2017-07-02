fe839d8f58f3f8fdb8a36c9e013a0bf3ec7b9892
xuanphuoc <wingame129@gmail.com>
2017-07-02 22:02:18 +0700

    app

---
+++ app/app.component.ts
@@ -0,0 +1,151 @@
+import { Component, ViewChild } from '@angular/core';
+
+import { Events, MenuController, Nav, Platform } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { Storage } from '@ionic/storage';
+
+import { AboutPage } from '../pages/about/about';
+import { AcountPage } from '../pages/acount/acount';
+import { LoginPage } from '../pages/login/login';
+import { MapPage } from '../pages/map/map';
+import { SignUpPage } from '../pages/sign-up/sign-up';
+import { TabsPage } from '../pages/tabs/tabs';
+import { TutorialPage } from '../pages/tutorial/tutorial';
+import { SchedulePage } from '../pages/schedule/schedule';
+import { SpeakerPage } from '../pages/speaker/speaker';
+import { SupportPage } from '../pages/support/support';
+
+import { ConferenceData } from '../providers/conference-data';
+import { UserData } from '../providers/user-data';
+
+export interface PageInterface {
+  title: string;
+  name: string;
+  component: any;
+  icon: string;
+  logsOut?: boolean;
+  index?: number;
+  tabName?: string;
+  tabComponent?: any;
+}
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  appPages: PageInterface[] = [
+    { title: 'Schedule', name: 'TabsPage', component: TabsPage, tabComponent: SchedulePage, index: 0, icon: 'calendar' },
+    { title: 'Speakers', name: 'TabsPage', component: TabsPage, tabComponent: SpeakerPage, index: 1, icon: 'contacts' },
+    { title: 'Map', name: 'TabsPage', component: TabsPage, tabComponent: MapPage, index: 2, icon: 'map' },
+    { title: 'About', name: 'TabsPage', component: TabsPage, tabComponent: AboutPage, index: 3, icon: 'information-circle' }
+  ];
+  loggedInPages: PageInterface[] = [
+    { title: 'Account', name: 'AccountPage', component: AcountPage, icon: 'person' },
+    { title: 'Support', name: 'SupportPage', component: SupportPage, icon: 'help' },
+    { title: 'Logout', name: 'TabsPage', component: TabsPage, icon: 'log-out', logsOut: true }
+  ];
+  loggedOutPages: PageInterface[] = [
+    { title: 'Login', name: 'LoginPage', component: LoginPage, icon: 'log-in' },
+    { title: 'Support', name: 'SupportPage', component: SupportPage, icon: 'help' },
+    { title: 'Signup', name: 'SignupPage', component: SignUpPage, icon: 'person-add' }
+  ];
+  rootPage: any;
+
+  constructor(
+    public events: Events,
+    public userData: UserData,
+    public menu: MenuController,
+    public platform: Platform,
+    public confData: ConferenceData,
+    public storage: Storage,
+    public splashScreen: SplashScreen
+  ) {
+
+    this.storage.get('hasSeenTutorial')
+      .then((hasSeenTutorial) => {
+        if (hasSeenTutorial) {
+          this.rootPage = TabsPage;
+        } else {
+          this.rootPage = TutorialPage;
+        }
+        this.platformReady()
+      });
+
+    confData.load();
+
+    this.userData.hasLoggedIn().then((hasLoggedIn) => {
+      this.enableMenu(hasLoggedIn === true);
+    });
+    this.enableMenu(true);
+
+    this.listenToLoginEvents();
+  }
+
+  openPage(page: PageInterface) {
+    let params = {};
+
+    if (page.index) {
+      params = { tabIndex: page.index };
+    }
+
+    if (this.nav.getActiveChildNav() && page.index != undefined) {
+      this.nav.getActiveChildNav().select(page.index);
+  } else {
+      this.nav.setRoot(page.name, params).catch((err: any) => {
+        console.log(`Didn't set nav root: ${err}`);
+      });
+    }
+
+    if (page.logsOut === true) {
+      this.userData.logout();
+    }
+  }
+
+  openTutorial() {
+    this.nav.setRoot(TutorialPage);
+  }
+
+  listenToLoginEvents() {
+    this.events.subscribe('user:login', () => {
+      this.enableMenu(true);
+    });
+
+    this.events.subscribe('user:signup', () => {
+      this.enableMenu(true);
+    });
+
+    this.events.subscribe('user:logout', () => {
+      this.enableMenu(false);
+    });
+  }
+
+  enableMenu(loggedIn: boolean) {
+    this.menu.enable(loggedIn, 'loggedInMenu');
+    this.menu.enable(!loggedIn, 'loggedOutMenu');
+  }
+
+  platformReady() {
+    this.platform.ready().then(() => {
+      this.splashScreen.hide();
+    });
+  }
+
+  isActive(page: PageInterface) {
+    let childNav = this.nav.getActiveChildNav();
+
+    if (childNav) {
+      if (childNav.getSelected() && childNav.getSelected().root === page.tabComponent) {
+        return 'primary';
+      }
+      return;
+    }
+
+    if (this.nav.getActive() && this.nav.getActive().name === page.name) {
+      return 'primary';
+    }
+    return;
+  }
+}
